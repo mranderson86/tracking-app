5afcb63389143d61433099f78f67ecedcf5cff32
mranderson86 <mr.anderson.brito@gmail.com>
2020-04-16 20:01:02 -0300

    Configurado tela de login

--- src/navigation/app.navigator.tsx
+++ src/navigation/app.navigator.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import { WelcomeScreen } from '../scenes/welcome.component';
-import { AppRoute } from './app-routes';
+import {createStackNavigator} from '@react-navigation/stack';
+import {WelcomeScreen} from '../scenes/welcome.component';
+import {AuthNavigator} from '../navigation/auth.navigator';
+import {AppRoute} from './app-routes';
 
 const Stack = createStackNavigator();
 
 export const AppNavigator = (props): React.ReactElement => (
-  <Stack.Navigator {...props} headerMode='none'>
-    <Stack.Screen name={AppRoute.AUTH} component={WelcomeScreen}/>
+  <Stack.Navigator {...props} headerMode="none">
+    <Stack.Screen name={AppRoute.AUTH} component={AuthNavigator} />
   </Stack.Navigator>
 );

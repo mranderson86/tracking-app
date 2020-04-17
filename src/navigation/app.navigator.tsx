00f92be594d15a05e82944ab968be6e7c5717ac2
mranderson86 <mr.anderson.brito@gmail.com>
2020-04-16 21:27:59 -0300

    Configurado rota Profile e Home

--- src/navigation/app.navigator.tsx
+++ src/navigation/app.navigator.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {AuthNavigator} from './auth.navigator';
-import {TodoNavigator} from './todo.navigator';
+import {HomeNavigator} from './home.navigator';
 import {AppRoute} from './app-routes';
 
 const Stack = createStackNavigator();
@@ -9,6 +9,6 @@ const Stack = createStackNavigator();
 export const AppNavigator = (props): React.ReactElement => (
   <Stack.Navigator {...props} headerMode="none">
     <Stack.Screen name={AppRoute.AUTH} component={AuthNavigator} />
-    <Stack.Screen name={AppRoute.HOME} component={TodoNavigator} />
+    <Stack.Screen name={AppRoute.HOME} component={HomeNavigator} />
   </Stack.Navigator>
 );
--- src/scenes/home/home-tab-bar.component.tsx
+++ src/scenes/home/home-tab-bar.component.tsx
@@ -1,9 +1,42 @@
 import React from 'react';
-import { View } from 'react-native';
+import {
+  BottomNavigation,
+  BottomNavigationTab,
+  Divider,
+  BottomNavigationTabElement,
+} from '@ui-kitten/components';
+import {
+  SafeAreaLayout,
+  SafeAreaLayoutElement,
+  SaveAreaInset,
+} from '../../components/safe-area-layout.component';
 
-export const HomeTabBar = (props): React.ReactElement => {
+export const HomeTabBar = (props): SafeAreaLayoutElement => {
+  const onSelect = (index: number): void => {
+    const selectedTabRoute: string = props.state.routeNames[index];
+    props.navigation.navigate(selectedTabRoute);
+  };
+
+  const createNavigationTabForRoute = (route): BottomNavigationTabElement => {
+    const {options} = props.descriptors[route.key];
+    return (
+      <BottomNavigationTab
+        key={route.key}
+        title={options.title}
+        icon={options.tabBarIcon}
+      />
+    );
+  };
 
   return (
-    <View/>
+    <SafeAreaLayout insets={SaveAreaInset.BOTTOM}>
+      <Divider />
+      <BottomNavigation
+        appearance="noIndicator"
+        selectedIndex={props.state.index}
+        onSelect={onSelect}>
+        {props.state.routes.map(createNavigationTabForRoute)}
+      </BottomNavigation>
+    </SafeAreaLayout>
   );
 };

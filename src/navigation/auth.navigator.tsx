b4786246a20b3eb6c969c61aa9a4387f38d4b797
mranderson86 <mr.anderson.brito@gmail.com>
2020-04-16 22:45:27 -0300

    Criado rota Autenticação

--- src/navigation/auth.navigator.tsx
+++ src/navigation/auth.navigator.tsx
@@ -3,6 +3,9 @@ import {createStackNavigator} from '@react-navigation/stack';
 import {AppRoute} from './app-routes';
 import {SignInScreen, SignUpScreen, ResetPasswordScreen} from '../scenes/auth';
 
+import {RouteProp} from '@react-navigation/core';
+import {StackNavigationProp} from '@react-navigation/stack';
+
 const Stack = createStackNavigator();
 
 export const AuthNavigator = (): React.ReactElement => (
@@ -15,3 +18,25 @@ export const AuthNavigator = (): React.ReactElement => (
     />
   </Stack.Navigator>
 );
+
+type AuthNavigatorParams = {
+  [AppRoute.SIGN_IN]: undefined;
+  [AppRoute.SIGN_UP]: undefined;
+  [AppRoute.RESET_PASSWORD]: undefined;
+  [AppRoute.HOME]: undefined;
+};
+
+export interface SignInScreenProps {
+  navigation: StackNavigationProp<AuthNavigatorParams, AppRoute.SIGN_IN>;
+  route: RouteProp<AuthNavigatorParams, AppRoute.SIGN_IN>;
+}
+
+export interface SignUpScreenProps {
+  navigation: StackNavigationProp<AuthNavigatorParams, AppRoute.SIGN_UP>;
+  route: RouteProp<AuthNavigatorParams, AppRoute.SIGN_UP>;
+}
+
+export interface ResetPasswordScreenProps {
+  navigation: StackNavigationProp<AuthNavigatorParams, AppRoute.RESET_PASSWORD>;
+  route: RouteProp<AuthNavigatorParams, AppRoute.RESET_PASSWORD>;
+}

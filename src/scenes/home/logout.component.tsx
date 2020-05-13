37b8b30d81276530175b131e18a1a270d42b3be8
mranderson86 <mr.anderson.brito@gmail.com>
2020-05-12 22:56:46 -0300

    Reseta estado da aplicação ao fazer log out

--- src/scenes/home/logout.component.tsx
+++ src/scenes/home/logout.component.tsx
@@ -2,11 +2,29 @@ import React from 'react';
 import {connect} from 'react-redux';
 
 import {LogoutTypes} from '../../store/ducks/authentication/types';
+import {TechnologiesUserTypes} from '../../store/ducks/technologies-user/types';
+import {UsersTechnologyTypes} from '../../store/ducks/users-technology/types';
+import {TechnologyTypes} from '../../store/ducks/technology/types';
 
 // Integrando Action em Props
 const mapDispatchToProps = {
   logoutRequest: () => ({
     type: LogoutTypes.LOGOUT_REQUEST,
+  }),
+
+  clearTechnologiesUser: () => ({
+    type: TechnologiesUserTypes.TECHNOLOGIES_USER_SUCCESS,
+    payload: [],
+  }),
+
+  clearUsersTechnology: () => ({
+    type: UsersTechnologyTypes.USERS_TECHNOLOGY_SUCCESS,
+    payload: [],
+  }),
+
+  clearTechnologies: () => ({
+    type: TechnologyTypes.TECHNOLOGY_SUCCESS,
+    payload: [],
   }),
 };
 
@@ -23,6 +41,8 @@ export const LogoutScreen = connect(
   null,
   mapDispatchToProps,
 )((props: Props) => {
+  props.clearTechnologiesUser();
+  props.clearUsersTechnology();
   props.logoutRequest();
   return <React.Fragment />;
 });
